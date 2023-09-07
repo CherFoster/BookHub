5bf210fdb0acd5684044b9e42793017334ad25a0
Cherise Foster <cherise.foster@outlook.com>
2023-09-07 14:04:39 -0600

    generated seed data for book

--- server/seed.py
+++ server/seed.py
@@ -5,6 +5,7 @@ from app import app
 from config import db
 from models import User, Book, Review
 from werkzeug.security import generate_password_hash
+import random
 
 if __name__ == '__main__':
     fake = Faker()
@@ -27,4 +28,46 @@ if __name__ == '__main__':
             db.session.add(user)
             users.append(user)
         db.session.commit()
+
+        books = []
+
+        book_genres = [
+            "Fiction",
+            "Non-Fiction",
+            "Mystery",
+            "Fantasy",
+            "Drama",
+            "Adventure",
+            "Cooking",
+            "Crime",
+            "Classic",
+            "Self-Help",
+            "Travel",
+            "Thriller",
+            "Poetry",
+            "Horror",
+            "Science Fiction",
+            "Biography",
+            "Romance",
+            "Young Adult (YA)"
+        ]
+
+        read_status = [
+            "Read",
+            "Want to read"
+        ]
+
+        for _ in range(60):
+            book = Book(
+                title = fake.catch_phrase(),
+                author = fake.name(),
+                genre = random.choice(book_genres),
+                description = fake.paragraph(nb_sentences=10),
+                status = random.choice(read_status)
+            )
+            db.session.add(book)
+            books.append(book)
+        db.session.commit()
+
+
 
