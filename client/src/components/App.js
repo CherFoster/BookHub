d3caac1a2ce45cb910d5a23def36292930a7afc3
Cherise Foster <cherise.foster@outlook.com>
2023-09-16 21:03:03 -0600

    added loginpage component and css

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,15 +2,26 @@ import React, { useEffect, useState } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import SignupForm from "./SignupForm";
 import LoginForm from "./LoginForm";
+import LoginPage from "./LoginPage";
 
 function App() {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    fetch("/check_session").then((res) => {
+      if (res.ok){
+        res.json().then((user) => setUser(user));
+      }
+    });
+  }, []);
+
+  if (!user) return <LoginPage onLogin={setUser} />;
 
   return (
     <div>
       <Router>
         <Routes>
-          <Route path="/signup" element={<SignupForm />} />
-          <Route path="/login" element={<LoginForm />} />
+          ?
         </Routes>
       </Router>
       
---
+++ client/src/components/LoginPage.js
@@ -0,0 +1,41 @@
+import '../styles/LoginPage.css';
+import { useState } from "react";
+import SignupForm from "./SignupForm";
+import LoginForm from "./LoginForm";
+
+function LoginPage({ onLogin }) {
+    const [showLogin, setShowLogin] = useState(true);
+
+    return (
+        <div className="login-page">
+            <div className="title-container">
+                <h1>Welcome to BookHub</h1>
+            </div>
+            {showLogin ? (
+                <>
+                    <LoginForm onLogin={onLogin}/>
+                    <br></br>
+                    <p>
+                      Don't have an account? 
+                        <button onClick={() => setShowLogin(false)}>
+                          Sign Up 
+                        </button>
+                    </p>
+                </>
+            ) : (
+                <>
+                    <SignupForm onLogin={onLogin} />
+                    <br></br>
+                    <p>
+                        Already have an account?
+                          <button onClick={() => setShowLogin(true)}>
+                            Log In
+                          </button>
+                    </p>
+                </>
+            )}
+        </div>
+    );
+}
+
+export default LoginPage;
